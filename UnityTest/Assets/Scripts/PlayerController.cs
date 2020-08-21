f202a5ec508150dc8def64f2099d120c16367509
Justin <justinkatic4@gmail.com>
2020-08-21 16:20:46 +1000

    roll a ball demo

---
+++ UnityTest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveForce = 10.0f;
+    public float jumpForce = 10.0f;
+
+    private Rigidbody rb;
+    private Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+        if(rb == null)
+        {
+            Debug.LogError("player Controller requires a rigidbody");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 inputForce = new Vector3();
+        inputForce.x = Input.GetAxis("Horizontal");
+        inputForce.z = Input.GetAxis("Vertical");
+        rb.AddForce(inputForce * moveForce);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Respawn")
+        {
+            transform.position = startPos;
+        }
+    }
+}
---
+++ UnityTest/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject objToSpawn;
+
+    public float spawninSeconds = 3.0f;
+    private float SpawnCooldown = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnCooldown = spawninSeconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnCooldown -= Time.deltaTime;
+        if(SpawnCooldown < 0)
+        {
+            SpawnCooldown = spawninSeconds;
+            var obj = GameObject.Instantiate(objToSpawn);
+            obj.transform.position = transform.position;
+        }
+    }
+}
